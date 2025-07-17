4872deac0c892ad1ea7990e9471de51b4cbf2b03
playdata\Playdata2 <dreamwars99@gmail.com>
2025-07-17 12:12:43 +0900

    Feat: evolution

--- LeeSeunghyeok/test.py
+++
@@ -1,9 +0,0 @@
-from paddleocr import PaddleOCR
- 
-ocr = PaddleOCR(lang="korean")
- 
-image_path = "test_image.png"
-result = ocr.ocr(image_path)
- 
-ocr_result = result[0]
-print('\n'.join(ocr_result['rec_texts']))
